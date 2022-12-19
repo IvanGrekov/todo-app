53c36850e58476e234fe5c746d3cfc1859ddedf3
Ivan Grekov <ivan.grekov@windmill.digital>
2022-12-19 21:43:18 +0200

    feat: routes created

---
+++ src/models/router.ts
@@ -0,0 +1,11 @@
+export enum ESearchParams {
+    PERIOD = 'period',
+}
+
+export enum EPeriodOptions {
+    TODAY = 'today',
+    TOMORROW = 'tomorrow',
+    THIS_WEEK = 'this-week',
+    THIS_MONTH = 'this-month',
+    UPCOMING = 'upcoming',
+}

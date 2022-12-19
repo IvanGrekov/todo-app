ae7bcbd65d221cc12407601f61553752ed8090da
Ivan Grekov <ivan.grekov@windmill.digital>
2022-12-19 21:43:05 +0200

    feat: BrowserRouter provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import ReactDOM from 'react-dom/client';
 
@@ -8,6 +9,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
 );

b7c26d6d05b67c1e08151e526dc4f3438fbe9329
Ivan Grekov <ivan.grekov@windmill.digital>
2022-12-24 15:14:22 +0200

    refactor: Header was refactored

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import 'components/header/Header.styles.scss';
 export default function Header(): JSX.Element {
     return (
         <header className="header">
-            <Typography element="h1" variant="h1" textAlign="left">
+            <Typography element="h1" variant="h1" style={{ textAlign: 'left' }}>
                 Todos
             </Typography>
 

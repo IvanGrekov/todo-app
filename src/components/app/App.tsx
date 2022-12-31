dd82449b49f24c66cbae322dd3fccc58ad8ef7a2
Ivan Grekov <ivan.grekov@windmill.digital>
2022-12-31 16:35:56 +0200

    feat: App component was updated

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,27 @@
+import ErrorBoundary from 'components/error-boundary';
 import Header from 'components/header';
+import NetworkErrors from 'components/network-errors';
 import PeriodFilter from 'components/period-filter';
 import Spacing from 'components/spacing';
+import { useNetworkErrors } from 'hooks/networkErrors.hooks';
+import NetworkErrorsContext from 'models/contexts/networkErrors';
 
 import 'components/app/App.styles.scss';
 
 export default function App(): JSX.Element {
+    const networkErrorsContextValue = useNetworkErrors();
+
     return (
         <main className="app">
-            <Header />
-            <Spacing sm={40} xl={60} />
-            <PeriodFilter />
-            <section />
+            <ErrorBoundary>
+                <NetworkErrorsContext.Provider value={networkErrorsContextValue}>
+                    <NetworkErrors />
+                    <Header />
+                    <Spacing sm={40} xl={60} />
+                    <PeriodFilter />
+                    <section />
+                </NetworkErrorsContext.Provider>
+            </ErrorBoundary>
         </main>
     );
 }

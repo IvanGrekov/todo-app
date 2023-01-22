2441ed93810baa6636b215b75ef0eca5934da56a
Ivan Grekov <ivan.grekov@windmill.digital>
2023-01-22 12:45:20 +0200

    style: colors were extended

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,5 +1,7 @@
 // NOTE: should be synchronized with styles/_variables.scss
 export const COLORS = {
+    primary: 'rgb(124, 191, 245)',
     white: 'rgb(235, 239, 243)',
     black: 'rgba(0, 0, 0, 0.7)',
+    red: 'rgba(171, 13, 13, 0.7)',
 };

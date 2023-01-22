c4ccd9bb293b56fff9f263ace6b9fdb9a6772785
Ivan Grekov <ivan.grekov@windmill.digital>
2023-01-22 12:47:22 +0200

    refactor: ErrorBoundary component was refactored

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -1,3 +1,43 @@
-import ErrorBoundary from 'components/error-boundary/ErrorBoundary';
+import { Component, ReactNode } from 'react';
 
-export default ErrorBoundary;
+import EmptyState from 'components/empty-state';
+import Spacing from 'components/spacing';
+
+interface IErrorBoundaryProps {
+    children: ReactNode;
+}
+
+interface IErrorBoundaryState {
+    hasError: boolean;
+}
+
+export default class ErrorBoundary extends Component<IErrorBoundaryProps, IErrorBoundaryState> {
+    constructor(props: IErrorBoundaryProps) {
+        super(props);
+        this.state = {
+            hasError: false,
+        };
+    }
+
+    public componentDidCatch(): void {
+        this.setState({ hasError: true });
+    }
+
+    public render(): ReactNode {
+        const { hasError } = this.state;
+
+        if (hasError) {
+            return (
+                <>
+                    <Spacing sm={60} xl={80} />
+                    <EmptyState
+                        isError={true}
+                        text="Error occurred on the page. Try to reload the page"
+                    />
+                </>
+            );
+        }
+
+        return this.props.children;
+    }
+}
